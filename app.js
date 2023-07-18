64c10503f1936ed450dafd4aaa3a03e13a3f0ce1
QamarFarooq <ch.farooq.qamar@hotmail.com>
2023-07-18 19:24:45 -0400

    setting up nodemon and express done

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("I am executed!!!!");
+});
+
+const server = http.createServer(app);
+
+server.listen(8081);
