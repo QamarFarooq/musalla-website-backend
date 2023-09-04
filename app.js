40d228d6c9cf365e82b8c10a68099446a08d43c1
QamarFarooq <ch.farooq.qamar@hotmail.com>
2023-09-04 17:14:52 -0400

    added basic hello world graphql functionality

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const http = require('http');
 
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+//const mySql = require('mySql');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("I am executed!!!!!!");
-});
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const { graphqlHTTP } = require('express-graphql');
+
+app.use('/graphql', graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver
+}));
+
+// app.use((req, res, next) => {
+//     console.log("I am executed!!!!!!");
+// });
 
 const server = http.createServer(app);
 
