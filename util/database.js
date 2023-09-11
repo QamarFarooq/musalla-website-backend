053a34892e5e6db0b5b578c068179652f31e2ee7
QamarFarooq <ch.farooq.qamar@hotmail.com>
2023-09-11 18:33:31 -0400

    added environemnt variables and also basic graphql functionalities

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'musallah_website_database',
+    password: `${process.env.MUSALLAH_WEBSITE_DATABASE_PASSWORD}`
+})
